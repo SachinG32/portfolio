89585092bfb9e256eff5b0b09f899e201c7901a2
Sachin Ghorpade <ghorpadesachin32@gmail.com>
2026-02-17 01:47:45 +0530

    updated fix for profile image and vercel issue

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Change to '/repository-name/' for GitHub Pages if not using custom domain
+  // Use '/' for Vercel, '/portfolio/' for GitHub Pages
+  base: process.env.VITE_BASE_PATH || '/',
 })
