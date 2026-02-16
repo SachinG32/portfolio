38f3cb7dfbf7d10c25c71e301070402e18a49e7c
Sachin Ghorpade <ghorpadesachin32@gmail.com>
2026-02-17 01:12:59 +0530

    modofied vite config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Change to '/repository-name/' for GitHub Pages if not using custom domain
+  base: '/portfolio/', // Change to '/repository-name/' for GitHub Pages if not using custom domain
 })
